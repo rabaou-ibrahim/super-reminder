724268266f9c4ba23a74ed6907d50298e5103639
rabaou-ibrahim <rabaou.ibrahim@laplateforme.io>
2023-12-04 15:53:11 +0100

    more comments

--- models/User/User.php
+++ models/User/User.php
@@ -18,28 +18,28 @@ class User {
     public function getId(): ?int {
         return $this->id;
     }
-
     public function setId(int $id): User {
         $this->id = $id;
 
         return $this;
     }
+
     public function getLastname(): ?string {
         return $this->lastname;
     }
-
     public function setLastname(string $lastname): User {
         $this->lastname = $lastname;
         return $this;
     }
+
     public function getFirstname(): ?string {
         return $this->firstname;
     }
-
     public function setFirstname(string $firstname): User {
         $this->firstname = $firstname;
         return $this;
     }
+
     public function getLogin(): ?string {
         return $this->login;
     }
@@ -51,7 +51,6 @@ class User {
     public function getPassword(): ?string  {
         return $this->password;
     } 
-
     public function setPassword(string $password): User  {
         $this->password = $password;
         return $this;
